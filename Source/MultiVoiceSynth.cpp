8501687f1122ef7da9afc2f25ea9dd24e0654839
Pit Fröhlich <pitfroehlich@hotmail.de>
2022-07-25 14:54:55 +0200

    external MultiVoiceSynth subClass now works

---
+++ Source/MultiVoiceSynth.cpp
@@ -0,0 +1,28 @@
+#include "MultiVoiceSynth.h"
+
+void MultiVoiceSynth::noteOn(const int midiChannel,
+    const int midiNoteNumber,
+    const float velocity)
+{
+    const juce::ScopedLock sl(lock);
+
+    for (auto* sound : sounds)
+    {
+        if (sound->appliesToNote(midiNoteNumber) && sound->appliesToChannel(midiChannel))
+        {
+            // If hitting a note that's still ringing, stop it first (it could be
+            // still playing because of the sustain or sostenuto pedal).
+            for (auto* voice : voices) {
+                if ((voice->getCurrentlyPlayingNote() == midiNoteNumber) &&
+                    voice->isPlayingChannel(midiChannel) &&
+                    (voice->getCurrentlyPlayingSound() == sound)
+                    ) {
+                    stopVoice(voice, 1.0f, true);
+                }
+            }
+
+            startVoice(findFreeVoice(sound, midiChannel, midiNoteNumber, isNoteStealingEnabled()),
+                sound, midiChannel, midiNoteNumber, velocity);
+        }
+    }
+}
--- Source/MultiVoiceSynth.h
+++ Source/MultiVoiceSynth.h
@@ -1,1 +1,7 @@
 #pragma once
+#include <JuceHeader.h>
+class MultiVoiceSynth : public juce::Synthesiser {
+    virtual void noteOn(int midiChannel,
+        int midiNoteNumber,
+        float velocity);
+};
