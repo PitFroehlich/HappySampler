13c31437e37298e24739a35e7f4e9a5d6ce8d408
Pit Fröhlich <pitfroehlich@hotmail.de>
2022-08-23 14:02:23 +0200

    delted unused method in sound

--- Source/HSamplerSound.h
+++ Source/HSamplerSound.h
@@ -18,8 +18,6 @@ public:
     HSamplerSound(const juce::String& name, juce::AudioFormatReader& source, const juce::BigInteger& midiNotes, int midiNoteForNormalPitch, double attackTimeSecs, double releaseTimeSecs, double maxSampleLengthSeconds) : SamplerSound(name, source, midiNotes, midiNoteForNormalPitch, attackTimeSecs, releaseTimeSecs, maxSampleLengthSeconds),
         sourceSampleRate(source.sampleRate), length(source.lengthInSamples), midiRootNote(midiNoteForNormalPitch) {}
 
-    void setGainControlParameters(GainControl::Parameters parametersToUse) { gainParams = parametersToUse; }
-
 private:
     friend class HSamplerVoice;
 
